1510635edd539f4d8113ad43900d67f65d91de2d
Cathy Dao <cathydao95@gmail.com>
2023-09-12 12:25:24 -0700

    add template vents components

--- client/src/components/event.jsx
+++ client/src/components/event.jsx
@@ -1,18 +1,17 @@
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
 const EventCard = (props) => {
-
-    return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Date: {!props.time ? "TBD" : null}</Card.Subtitle>
-                <Card.Text>
-                    Location: {props.location}
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    )
-}
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          Date: {!props.time ? "TBD" : null}
+        </Card.Subtitle>
+        <Card.Text>Location: {props.location}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default EventCard;
