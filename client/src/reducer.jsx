1e51d95c78b2726fa5d69619847d47ea0fa52510
Cathy Dao <cathydao95@gmail.com>
2023-09-12 22:54:59 -0700

    create reducer

---
+++ client/src/reducer.jsx
@@ -0,0 +1,53 @@
+import {
+  SHOW_MODAL,
+  CLOSE_MODAL,
+  ADD_EVENT,
+  DELETE_EVENT,
+  GET_ALL_EVENTS,
+  HANDLE_CHANGE,
+  EDIT_EVENT,
+} from "./actions";
+
+const reducer = (state, action) => {
+  const { showModal, editJobId } = state;
+  if (action.type === SHOW_MODAL) {
+    return { ...state, showModal: true, editJobId: action.payload.id };
+  }
+  if (action.type === CLOSE_MODAL) {
+    return { ...state, showModal: false, editJobId: action.payload.id };
+  }
+
+  if (action.type === HANDLE_CHANGE) {
+    const event = state.event;
+    const newEvent = { ...event, [action.payload.name]: action.payload.value };
+    console.log("newEvent", newEvent);
+
+    return { ...state, event: newEvent };
+  }
+
+  if (action.type === GET_ALL_EVENTS) {
+    const events = action.payload.eventsData;
+    return { ...state, events: events };
+  }
+
+  if (action.type === ADD_EVENT) {
+    const events = state.events;
+    const newEvent = action.payload.event;
+    return { ...state, events: [...events, newEvent] };
+  }
+
+  // if (action.type === EDIT_EVENT) {
+  //   const events = state.events;
+  //   const updatedEvent = action.payload.event;
+  //   return { ...state, events: [...events, updatedEvent] };
+  // }
+
+  if (action.type === DELETE_EVENT) {
+    const events = state.events;
+    const eventId = action.payload.id;
+    const updatedEvents = events.filter((event) => event.id !== eventId);
+    return { ...state, events: updatedEvents };
+  }
+};
+
+export default reducer;
