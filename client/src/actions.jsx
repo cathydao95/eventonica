4ee67d9f73bce31cc295e5dcf3102381651204a8
Cathy Dao <cathydao95@gmail.com>
2023-09-12 22:54:37 -0700

    create actions

---
+++ client/src/actions.jsx
@@ -0,0 +1,8 @@
+export const SHOW_MODAL = "SHOW_MODAL";
+export const CLOSE_MODAL = "CLOSE_MODAL";
+export const ADD_EVENT = "ADD_EVENT";
+export const DELETE_EVENT = "DELETE_EVENT";
+export const EDIT_EVENT = "EDIT_EVENT";
+export const GET_ALL_EVENTS = "GET ALL EVENTS";
+export const HANDLE_CHANGE = "HANDLE_CHANGE";
+export const SET_EDIT_EVENT = "SET_EDIT_EVENT";
