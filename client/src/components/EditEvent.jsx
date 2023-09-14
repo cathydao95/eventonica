234788f6cd571c0880bf6ec84b580cff699d72df
Cathy Dao <cathydao95@gmail.com>
2023-09-13 17:09:18 -0700

    remove unused functions

--- client/src/components/EditEvent.jsx
+++ client/src/components/EditEvent.jsx
@@ -13,20 +13,32 @@ const EditEvent = ({ singleEvent }) => {
   } = useGlobalContext();
   const { id } = singleEvent;
 
-  const updateEventInfo = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    handleChange(name, value);
-  };
-
   function formatEventTime(eventTime) {
     const date = new Date(eventTime);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(date.getUTCDate()).padStart(2, "0");
 
     return `${year}-${month}-${day}`;
   }
+
+  const [eventInfo, setEventInfo] = useState({
+    title: singleEvent.title,
+    location: singleEvent.location,
+    eventtime: singleEvent.eventtime,
+  });
+
+  const [formattedDate, setFormattedDate] = useState(
+    formatEventTime(eventInfo.eventtime)
+  );
+
+  console.log(formattedDate);
+
+  const updateEventInfo = (e) => {
+    setEventInfo((prevInfo) => {
+      return { ...prevInfo, [e.target.name]: e.target.value };
+    });
+  };
 
   return (
     <div>
@@ -52,7 +64,7 @@ const EditEvent = ({ singleEvent }) => {
                 placeholder="Event Name"
                 name="title"
                 onChange={updateEventInfo}
-                // value={singleEvent.title}
+                value={eventInfo.title}
               />
             </div>
             <div>
@@ -63,7 +75,7 @@ const EditEvent = ({ singleEvent }) => {
                 placeholder="Location"
                 name="location"
                 onChange={updateEventInfo}
-                // value={singleEvent.location}
+                value={eventInfo.location}
               />
             </div>
             <div>
@@ -74,12 +86,12 @@ const EditEvent = ({ singleEvent }) => {
                 placeholder="Event Date"
                 name="eventtime"
                 onChange={updateEventInfo}
-                // value={formatEventTime(singleEvent.eventtime)}
+                value={formattedDate}
               />
             </div>
           </form>
           <div className="btnContainer">
-            <button onClick={() => updateEvent(id)}>Edit</button>
+            <button onClick={() => updateEvent(id, eventInfo)}>Edit</button>
             <button onClick={() => closeEditModal(id)}>Cancel</button>
           </div>
         </div>
