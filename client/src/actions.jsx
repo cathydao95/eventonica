f21c9e5dbc62b2d86f2ad242865ceaef75be530d
Cathy Dao <cathydao95@gmail.com>
2023-09-13 17:08:29 -0700

    remove unused actions

--- client/src/actions.jsx
+++ client/src/actions.jsx
@@ -4,5 +4,4 @@ export const ADD_EVENT = "ADD_EVENT";
 export const DELETE_EVENT = "DELETE_EVENT";
 export const EDIT_EVENT = "EDIT_EVENT";
 export const GET_ALL_EVENTS = "GET ALL EVENTS";
-export const HANDLE_CHANGE = "HANDLE_CHANGE";
 export const SET_EDIT_EVENT = "SET_EDIT_EVENT";
