3a334e68f18162707804e8d49be5fbc1d8ae7ad0
Cathy Dao <cathydao95@gmail.com>
2023-09-13 17:09:34 -0700

    update state to take in accounts daylights savings

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -7,9 +7,9 @@ const EventCard = ({ singleEvent }) => {
   const { title, location, eventtime, id } = singleEvent;
 
   const date = new Date(eventtime);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
-  const day = String(date.getDate()).padStart(2, "0");
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const day = String(date.getUTCDate()).padStart(2, "0");
 
   const formattedDate = `${month}/${day}/${year}`;
 
